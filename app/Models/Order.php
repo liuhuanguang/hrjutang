2ede316d50be0bfe94ce8e805f56e080289a5fea
user <923414405@qq.com>
2018-08-24 12:02:44 +0800

    update

---
+++ app/Models/Order.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/24
+ * Time: 12:01
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+class Order extends Model
+{
+    protected $table = 'order'; //指定数据表名称
+}
